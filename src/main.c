32df81fec1e0f96f9e40e2f1221906ca9e976aa1
Tom Copeland <thcopeland2001@gmail.com>
2021-12-15 21:37:47 -0500

    Remove now-unneeded comments

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "display.h"
 #include "map.h"
 #include "tile.h"
-#include "coords.h"
 #include "render.h"
 #include "main.h"
 
@@ -55,11 +54,9 @@ ISR(TIMER1_COMPA_vect, ISR_NAKED) {
         // ~90 free cycles
         // render a single layer of the footer (120x16)
     } else if (current_stage == 2) {
-        // game_data.active_sector = sectors;
         tmp++;
         if (tmp > 0) {
             tmp = 0;
-            // x++;
             PORTB ^= 0b10000000;
             if (dir_x) {
                 if (game_data.offset_x_l < 11) {
@@ -103,9 +100,7 @@ ISR(TIMER1_COMPA_vect, ISR_NAKED) {
                 }
             }
         }
-        // if (x > 11) x = 0;
         render_sector(fbuff, game_data.active_sector, game_data.offset_x_h, game_data.offset_x_l, game_data.offset_y_h, game_data.offset_y_l);
-        // render_sector(fbuff, game_data.active_sector, 0, 0, game_data.offset_y_h, game_data.offset_y_l);
         current_stage++;
         TIFR1 = 0xFF;
     } else if (current_stage > 2) {
@@ -119,22 +114,6 @@ ISR(TIMER1_COMPA_vect, ISR_NAKED) {
 }
 
 int main(void) {
-    // for (uint16_t i = 0; i < sizeof(fbuff); i++) {
-    //     fbuff[i] = i;
-    // }
-    //
-    // fbuff[0] = 0xc7; fbuff[1] = 0xc7; fbuff[2] = 0xd; fbuff[3] = 0xd; fbuff[4] = 0x48; fbuff[5] = 0x48; fbuff[6] = 0x48; fbuff[7] = 0x48; fbuff[8] = 0x48; fbuff[9] = 0xc7; fbuff[10] = 0xc7; fbuff[11] = 0xc7;
-    // fbuff[120] = 0xc7; fbuff[121] = 0x48; fbuff[122] = 0x48; fbuff[123] = 0x2f; fbuff[124] = 0xd1; fbuff[125] = 0xd1; fbuff[126] = 0xd1; fbuff[127] = 0xd1; fbuff[128] = 0x48; fbuff[129] = 0x48; fbuff[130] = 0x48; fbuff[131] = 0xc7;
-    // fbuff[240] = 0xc7; fbuff[241] = 0xc7; fbuff[242] = 0xa; fbuff[243] = 0x48; fbuff[244] = 0x48; fbuff[245] = 0xd1; fbuff[246] = 0xd1; fbuff[247] = 0x48; fbuff[248] = 0x48; fbuff[249] = 0xa; fbuff[250] = 0xc7; fbuff[251] = 0xc7;
-    // fbuff[360] = 0xc7; fbuff[361] = 0xc7; fbuff[362] = 0x13; fbuff[363] = 0x6e; fbuff[364] = 0x6e; fbuff[365] = 0x48; fbuff[366] = 0x48; fbuff[367] = 0x6e; fbuff[368] = 0x6e; fbuff[369] = 0x13; fbuff[370] = 0xc7; fbuff[371] = 0xc7;
-    // fbuff[480] = 0xc7; fbuff[481] = 0xc7; fbuff[482] = 0x5d; fbuff[483] = 0xa; fbuff[484] = 0xa; fbuff[485] = 0x6e; fbuff[486] = 0x6e; fbuff[487] = 0xa; fbuff[488] = 0xa; fbuff[489] = 0x5d; fbuff[490] = 0xc7; fbuff[491] = 0xc7;
-    // fbuff[600] = 0xc7; fbuff[601] = 0xc7; fbuff[602] = 0x5d; fbuff[603] = 0xff; fbuff[604] = 0x48; fbuff[605] = 0x6e; fbuff[606] = 0x6e; fbuff[607] = 0x48; fbuff[608] = 0xff; fbuff[609] = 0x5d; fbuff[610] = 0xc7; fbuff[611] = 0xc7;
-    // fbuff[720] = 0xc7; fbuff[721] = 0xc7; fbuff[722] = 0xc7; fbuff[723] = 0x5d; fbuff[724] = 0x5d; fbuff[725] = 0x5d; fbuff[726] = 0x5d; fbuff[727] = 0x5d; fbuff[728] = 0x5d; fbuff[729] = 0xc7; fbuff[730] = 0xc7; fbuff[731] = 0xc7;
-    // fbuff[840] = 0xc7; fbuff[841] = 0xc7; fbuff[842] = 0x77; fbuff[843] = 0x2f; fbuff[844] = 0x48; fbuff[845] = 0xd1; fbuff[846] = 0xd1; fbuff[847] = 0x48; fbuff[848] = 0x2f; fbuff[849] = 0x77; fbuff[850] = 0xc7; fbuff[851] = 0xc7;
-    // fbuff[960] = 0xc7; fbuff[961] = 0xc7; fbuff[962] = 0x48; fbuff[963] = 0x48; fbuff[964] = 0xd1; fbuff[965] = 0x48; fbuff[966] = 0x48; fbuff[967] = 0xd1; fbuff[968] = 0x48; fbuff[969] = 0x48; fbuff[970] = 0xc7; fbuff[971] = 0xc7;
-    // fbuff[1080] = 0xc7; fbuff[1081] = 0xc7; fbuff[1082] = 0x5d; fbuff[1083] = 0x6e; fbuff[1084] = 0x48; fbuff[1085] = 0x48; fbuff[1086] = 0x48; fbuff[1087] = 0x48; fbuff[1088] = 0x6e; fbuff[1089] = 0x5d; fbuff[1090] = 0xc7; fbuff[1091] = 0xc7;
-    // fbuff[1200] = 0xc7; fbuff[1201] = 0xc7; fbuff[1202] = 0xc7; fbuff[1203] = 0x48; fbuff[1204] = 0x48; fbuff[1205] = 0xa; fbuff[1206] = 0xa; fbuff[1207] = 0x48; fbuff[1208] = 0x48; fbuff[1209] = 0xc7; fbuff[1210] = 0xc7; fbuff[1211] = 0xc7;
-    // fbuff[1320] = 0xc7; fbuff[1321] = 0xc7; fbuff[1322] = 0xc7; fbuff[1323] = 0xa; fbuff[1324] = 0xa; fbuff[1325] = 0xc7; fbuff[1326] = 0xc7; fbuff[1327] = 0xa; fbuff[1328] = 0xa; fbuff[1329] = 0xc7; fbuff[1330] = 0xc7; fbuff[1331] = 0xc7;
     DDRA = 0xFF;
     DDRB = 0xFF;
     DDRE = 0xFF;
--- src/render.c
+++ src/render.c
@@ -5,9 +5,7 @@
 
 void render_whole_tile(uint8_t *buff, uint8_t tile, uint8_t min_y, uint8_t height) {
     const __flash uint8_t *img = tileset[tile] + min_y * TILE_WIDTH;
-    // uint16_t img = (uint16_t) tileset[tile] + min_y * TILE_WIDTH;
 
-    // for (int i = 0; i < height; i++) {
     while (height--) {
         asm volatile(
             "lpm __tmp_reg__, %a0+\n\t"
@@ -37,107 +35,15 @@ void render_whole_tile(uint8_t *buff, uint8_t tile, uint8_t min_y, uint8_t heigh
             : "+z" (img), "+e" (buff)
             : : "r0"
         );
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
-        // *(buff++) = *(img++);
         buff += DISPLAY_WIDTH-TILE_WIDTH;
     }
 }
-
-/*void render_partial_tile(uint8_t *buff, uint8_t tile,
-                         uint8_t min_y, uint8_t height,
-                         uint8_t x_edge, uint8_t direction) {
-    #ifdef DEV
-    // 12 or higher results in a bad jump
-    if (x_edge > 11) {
-        // for (uint8_t i = 0; i < height; i++) {
-        //     for (uint8_t j = 0; j < x_edge; j++) *(buff++) = 0x7;
-        //     buff += DISPLAY_WIDTH-x_edge;
-        // }
-        return;
-    }
-    #endif
-    // PERF this could be faster if written entirely in assembly
-    // inline assembly might work, but the C/ASM labels don't mix well.
-    // the main idea is to store jmp on the stack and use ret to jump.
-    //    asm volatile(
-    //        "sbi %0, 0x1\n\t"
-    //        "breq end\n\t"
-    //        "add %A1, %2\n\t"
-    //        "adc %B1, __zero_reg__\n\t"
-    //        "push %A3\n\t"
-    //        "push %B3\n\t"
-    //        "ret\n\t"
-    // "end:   nop\n\t"
-    //        : "+r" (height), "+z" (img)
-    //        : "r" (min_x), "r" (jmp)
-    //    );
-    const __flash uint8_t *img = tileset[tile] + min_y * TILE_WIDTH;
-    static const void *jmps[12] = { &&i, &&ii, &&iii, &&iv, &&v, &&vi, &&vii, &&viii, &&ix, &&x, &&xi, &&xii };
-    const void *jmp = jmps[x_edge];
-
-    // uint8_t width = TILE_WIDTH - x_edge;
-    // if (direction) width = x_edge;
-    // uint8_t width = x_edge;
-    // if (direction) width = TILE_WIDTH - x_edge;
-    //
-    // for (uint8_t i = 0; i < height; i++) {
-    //     for (uint8_t j = 0; j < width; j++) {
-    //         *(buff++) = *(img++);
-    //     }
-    //     buff += DISPLAY_WIDTH - width;
-    //     img += x_edge;
-    // }
-    // return;
-
-    if (direction) img += x_edge;
-    // while (height--) {
-    for (int i = 0; i < height; i++) {
-        goto *jmp;
-// i:      asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// ii:     asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// iii:    asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// iv:     asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// v:      asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// vi:     asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// vii:    asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// viii:   asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// ix:     asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// x:      asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// xi:     asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-// xii:    asm volatile("lpm __tmp_reg__, %a0+\n\tst %a1+, __tmp_reg__\n\t" : "+z" (img), "+e" (buff));
-i:      *(buff++) = *(img++);
-ii:     *(buff++) = *(img++);
-iii:    *(buff++) = *(img++);
-iv:     *(buff++) = *(img++);
-v:      *(buff++) = *(img++);
-vi:     *(buff++) = *(img++);
-vii:    *(buff++) = *(img++);
-viii:   *(buff++) = *(img++);
-ix:     *(buff++) = *(img++);
-x:      *(buff++) = *(img++);
-xi:     *(buff++) = *(img++);
-xii:    *(buff++) = *(img++);
-        buff += DISPLAY_WIDTH - (TILE_WIDTH - x_edge);
-        img += x_edge;
-    }
-}*/
 
 void render_partial_tile2(uint8_t *buff, uint8_t tile,
                           uint8_t min_y, uint8_t height,
                           uint8_t min_x, uint8_t width) {
     const __flash uint8_t *img = tileset[tile] + min_y * TILE_WIDTH + min_x;
 
-    // for (uint8_t i = 0; i < height; i++) {
     while (height--) {
         for (uint8_t j = 0; j < width; j++) {
             *(buff++) = *(img++);
@@ -158,7 +64,6 @@ void render_sector(uint8_t *fbuff,
         uint8_t *buff = fbuff;
         // upper left corner
         uint8_t corner = SECTOR_WIDTH*offset_y_h + offset_x_h;
-        // render_partial_tile(buff, sector->tiles[corner], offset_y_l, TILE_HEIGHT-offset_y_l, offset_x_l, 1);
         render_partial_tile2(buff, sector->tiles[corner], offset_y_l, TILE_HEIGHT-offset_y_l, offset_x_l, TILE_WIDTH-offset_x_l);
 
         // top and bottom edges
@@ -173,42 +78,24 @@ void render_sector(uint8_t *fbuff,
 
         // upper right corner
         corner = SECTOR_WIDTH*offset_y_h + offset_x_h + DISPLAY_TILE_WIDTH;
-        // render_partial_tile(buff, sector->tiles[corner], offset_y_l, TILE_HEIGHT-offset_y_l, TILE_WIDTH-offset_x_l, 0);
         render_partial_tile2(buff, sector->tiles[corner], offset_y_l, TILE_HEIGHT-offset_y_l, 0, offset_x_l);
 
         // left and right edges
-        // buff = fbuff + (TILE_HEIGHT-offset_y_l)*DISPLAY_WIDTH;
-        // for (uint8_t row = 1; row < DISPLAY_TILE_HEIGHT; row++) {
-        //     uint8_t left = SECTOR_WIDTH*(row+offset_y_h) + offset_x_h,
-        //             right = left + DISPLAY_TILE_WIDTH;
-        //     render_partial_tile(buff, sector->tiles[left], 0, 12, offset_x_l, 1);
-        //     render_partial_tile(buff+DISPLAY_WIDTH-offset_x_l, sector->tiles[right], 0, 12, TILE_WIDTH-offset_x_l, 0);
-        //     buff += DISPLAY_WIDTH*TILE_WIDTH;
-        // }
         buff = fbuff + (TILE_HEIGHT-offset_y_l)*DISPLAY_WIDTH;
         for (uint8_t row = 1; row < DISPLAY_TILE_HEIGHT; row++) {
             uint8_t left = SECTOR_WIDTH*(row+offset_y_h) + offset_x_h,
                     right = left + DISPLAY_TILE_WIDTH;
             render_partial_tile2(buff, sector->tiles[left], 0, 12, offset_x_l, TILE_WIDTH-offset_x_l);
             render_partial_tile2(buff+DISPLAY_WIDTH-offset_x_l, sector->tiles[right], 0, 12, 0, offset_x_l);
-            // render_partial_tile2(buff, sector->tiles[left], 0, 12, 0, 12);
-            // render_partial_tile(buff+DISPLAY_WIDTH-offset_x_l, sector->tiles[right], 0, 12, TILE_WIDTH-offset_x_l, 0);
             buff += DISPLAY_WIDTH*TILE_WIDTH;
         }
 
         // lower left corner
-        // corner = SECTOR_WIDTH*(offset_y_h+DISPLAY_TILE_HEIGHT) + offset_x_h;
-        // buff = fbuff + DISPLAY_WIDTH*(DISPLAY_HEIGHT-offset_y_l);
-        // buff = fbuff + DISPLAY_WIDTH*(DISPLAY_HEIGHT-12);
-        // render_partial_tile(fbuff+24, 2, 0, 6, 6, 1);
         corner = SECTOR_WIDTH*(offset_y_h+DISPLAY_TILE_HEIGHT) + offset_x_h;
         buff = fbuff + DISPLAY_WIDTH*(DISPLAY_HEIGHT-offset_y_l);
         render_partial_tile2(buff, sector->tiles[corner], 0, offset_y_l, offset_x_l, TILE_WIDTH-offset_x_l);
-        //
-        // // lower right corner
-        // corner = SECTOR_WIDTH*(offset_y_h+DISPLAY_TILE_HEIGHT) + offset_x_h + DISPLAY_TILE_WIDTH;
-        // buff += DISPLAY_WIDTH - offset_x_l;
-        // render_partial_tile(buff, sector->tiles[corner], 0, offset_y_l, TILE_WIDTH-offset_x_l, 0);
+
+        // lower right corner
         corner = SECTOR_WIDTH*(offset_y_h+DISPLAY_TILE_HEIGHT) + offset_x_h + DISPLAY_TILE_WIDTH;
         buff += DISPLAY_WIDTH - offset_x_l;
         render_partial_tile2(buff, sector->tiles[corner], 0, offset_y_l, 0, offset_x_l);
@@ -223,8 +110,6 @@ void render_sector(uint8_t *fbuff,
                     right = left + DISPLAY_TILE_WIDTH;
             render_partial_tile2(buff, sector->tiles[left], 0, 12, offset_x_l, TILE_WIDTH-offset_x_l);
             render_partial_tile2(buff+DISPLAY_WIDTH-offset_x_l, sector->tiles[right], 0, 12, 0, offset_x_l);
-            // render_partial_tile(buff, sector->tiles[left], 0, 12, TILE_WIDTH-offset_x_l, 1);
-            // render_partial_tile(buff+DISPLAY_WIDTH-TILE_WIDTH+offset_x_l, sector->tiles[right], 0, 12, offset_x_l, 0);
             buff += DISPLAY_WIDTH*TILE_HEIGHT;
         }
 
